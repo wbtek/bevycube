79361f48c719c7641caf8a3f67adaf8ea2233785
Greg Slocum <fromgit@wbtek.slocum.net>
2026-01-20 15:33:44 +0900

    rotate cube

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct RotatingCube;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, rotate_cube)
         .run();
 }
 
@@ -17,6 +20,7 @@ fn setup(
         Mesh3d(meshes.add(Cuboid::default())),
         MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
         Transform::from_xyz(0.0, 0.5, 0.0),
+        RotatingCube,
     ));
     // Light
     commands.spawn(PointLight::default());
@@ -25,4 +29,10 @@ fn setup(
         Camera3d::default(),
         Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
+}
+
+fn rotate_cube(mut query: Query<&mut Transform, With<RotatingCube>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(1.0 * time.delta_secs());
+    }
 }

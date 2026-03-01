e157cf45987b4ba9b45fa907f4b4eda7c710cae9
Greg Slocum <fromgit@wbtek.slocum.net>
2026-03-02 01:24:33 +0900

    make viewport pay a little more attention to window size

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,21 @@ fn main() {
     .insert_resource(world::cube::CubeParms {
       rotation_speed: -1.0,
     })
-    .add_plugins(DefaultPlugins.set(AssetPlugin {
-      meta_check: AssetMetaCheck::Never,
-      ..default()
-    }))
+    .add_plugins(
+      DefaultPlugins
+        .set(WindowPlugin {
+          primary_window: Some(Window {
+            fit_canvas_to_parent: true,
+            prevent_default_event_handling: true,
+            ..default()
+          }),
+          ..default()
+        })
+        .set(AssetPlugin {
+          meta_check: AssetMetaCheck::Never,
+          ..default()
+        }),
+    )
     .add_plugins(MeshPickingPlugin)
     .add_plugins((
       EmbeddedAssetsPlugin,
